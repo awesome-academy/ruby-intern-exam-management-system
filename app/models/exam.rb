58a6171a8c944af412e5b362e7c0a51715760600
phvanhoang <hoangpv6681@gmail.com>
2021-04-16 16:36:00 +0700

    View Exams List

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,3 +1,4 @@
 class Exam < ApplicationRecord
   belongs_to :subject
+  scope :sort_by_created_at_desc, ->{order(created_at: :desc)}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
     namespace :supervisor do
       resources :questions
+      resources :exams
     end
   end
 end
