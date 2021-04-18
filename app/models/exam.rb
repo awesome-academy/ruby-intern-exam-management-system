43ab161b4883fea79ddbd1b3284ab5945023b0f9
phvanhoang <hoangpv6681@gmail.com>
2021-04-19 00:05:28 +0700

    View Exam Detail

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,4 +1,9 @@
 class Exam < ApplicationRecord
   belongs_to :subject
   scope :sort_by_created_at_desc, ->{order(created_at: :desc)}
+  has_many :exam_questions, dependent: :destroy
+  has_many :questions, through: :exam_questions
+  scope :questions_sort_by_created_at_asc, (lambda do |exam|
+    exam.questions.order(created_at: :asc)
+  end)
 end
